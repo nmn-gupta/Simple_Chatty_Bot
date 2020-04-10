6ea1ec13b0d9e742e45b8f0f89e47a50cd6d4854
naman <naman.gupta_cs18@gla.ac.in>
2020-04-10 14:56:35 +0530

    creating object of Task1 class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,13 @@
 package main;
 
+import task1.Task1;
+
 public class Main {
     public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        task1.greet("Aid", 2018);
+
+
 
     }
 }
