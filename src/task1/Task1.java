f98b1d2e351ccf7627be2a567c3412b932982b74
naman <naman.gupta_cs18@gla.ac.in>
2020-04-10 13:27:58 +0530

    create object of  scanner class

--- src/task1/Task1.java
+++ src/task1/Task1.java
@@ -1,7 +1,10 @@
 package task1;
 
+import java.util.Scanner;
+
 public class Task1 {
-    public void greet(String assistantName, int birthYear) {
+    public void greet(String botName, int year) {
+        Scanner scanner = new Scanner(System.in);
     }
 
 }
