431bcdb30a4076d35ebf04087d32cd8fd97b725a
naman <naman.gupta_cs18@gla.ac.in>
2020-04-10 13:54:35 +0530

    creating object of Scanner class

--- src/task3/Task3.java
+++ src/task3/Task3.java
@@ -1,12 +1,14 @@
 package task3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public void operation() {
         System.out.println("Now I will prove  you that I can do any task you want from listed below:");
         System.out.println("Press 1. to check whether the given number is prime or not. ");
         System.out.println("Press 2. to check whether the given number is odd or even. ");
         System.out.println("Press 3. to find theb factorial of any number you want. ");
-
+        Scanner sc = new Scanner(System.in);
 
     }
 }
