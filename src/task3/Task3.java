5f438cff051fa7501dc676250ae475a729fd8af2
naman <naman.gupta_cs18@gla.ac.in>
2020-04-10 14:02:57 +0530

    completed the method checkPrime()

--- src/task3/Task3.java
+++ src/task3/Task3.java
@@ -3,12 +3,13 @@ package task3;
 import java.util.Scanner;
 
 public class Task3 {
+    Scanner sc = new Scanner(System.in);
     public void operation() {
         System.out.println("Now I will prove  you that I can do any task you want from listed below:");
         System.out.println("Press 1. to check whether the given number is prime or not. ");
         System.out.println("Press 2. to check whether the given number is odd or even. ");
         System.out.println("Press 3. to find theb factorial of any number you want. ");
-        Scanner sc = new Scanner(System.in);
+
         int choice = sc.nextInt();
         if (choice == 1) {
             checkPrime();
@@ -25,6 +26,23 @@ public class Task3 {
     }
 
     public void checkPrime() {
+
+        System.out.println("Enter the number to be checked:");
+        int userInput = sc.nextInt();
+        int c = 0;
+        for (int i = 2; i < userInput; i++) {
+            if (userInput % i == 0) {
+                c++;
+            }
+
+        }
+        if (c == 0) {
+            System.out.println(userInput + " is Prime number");
+
+        } else {
+            System.out.println(userInput + " is not a Prime number");
+
+        }
 
     }
 }
