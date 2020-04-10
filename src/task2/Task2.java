ee562d5158759f86deb5751e44bc316593b1c434
naman <naman.gupta_cs18@gla.ac.in>
2020-04-10 13:37:58 +0530

    generate getter and setter methods for firstRemainder

--- src/task2/Task2.java
+++ src/task2/Task2.java
@@ -4,4 +4,12 @@ public class Task2 {
     private int firstRemainder;
     private int secondRemainder;
     private int thirdRemainder;
+
+    public int getFirstRemainder() {
+        return firstRemainder;
+    }
+
+    public void setFirstRemainder(int firstRemainder) {
+        this.firstRemainder = firstRemainder;
+    }
 }
