f1c40ad9c14a78e27395014df7106e05c54be24c
naman <naman.gupta_cs18@gla.ac.in>
2020-04-10 13:53:52 +0530

    adding statements

--- src/task3/Task3.java
+++ src/task3/Task3.java
@@ -2,6 +2,11 @@ package task3;
 
 public class Task3 {
     public void operation() {
+        System.out.println("Now I will prove  you that I can do any task you want from listed below:");
+        System.out.println("Press 1. to check whether the given number is prime or not. ");
+        System.out.println("Press 2. to check whether the given number is odd or even. ");
+        System.out.println("Press 3. to find theb factorial of any number you want. ");
+
 
     }
 }
