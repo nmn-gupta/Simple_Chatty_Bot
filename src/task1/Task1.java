29daa93b52bfe3d7ad609c8c66535ab839131858
naman <naman.gupta_cs18@gla.ac.in>
2020-04-10 13:31:28 +0530

    printing messages

--- src/task1/Task1.java
+++ src/task1/Task1.java
@@ -3,8 +3,12 @@ package task1;
 import java.util.Scanner;
 
 public class Task1 {
-    public void greet(String botName, int year) {
+    public void greet(String assistantName, int birthYear) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+        String name = scanner.nextLine();
     }
 
 }
