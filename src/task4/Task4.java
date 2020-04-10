481172be08a023311d79370d90dd9ef2ebffb79a
naman <naman.gupta_cs18@gla.ac.in>
2020-04-10 14:39:11 +0530

    creating object of Scanner class

--- src/task4/Task4.java
+++ src/task4/Task4.java
@@ -1,7 +1,14 @@
 package task4;
 
+import java.util.Scanner;
+
 public class Task4 {
     public void test() {
+        System.out.println("Let's test your calculation speed");
+        int num1 = (int) (Math.random() * 100);
+        int num2 = (int) (Math.random() * 100);
+        System.out.println(num1 + " + " + num2 + " = ");
+        Scanner sc = new Scanner(System.in);
 
     }
 }
