eb213d8591887e33359eb63b8d59ec547b80e603
naman <naman.gupta_cs18@gla.ac.in>
2020-04-10 14:52:28 +0530

    apply check conditions

--- src/task4/Task4.java
+++ src/task4/Task4.java
@@ -9,20 +9,37 @@ public class Task4 {
         int num2 = (int) (Math.random() * 100);
         System.out.println(num1 + " * " + num2 + " = ");
         Scanner sc = new Scanner(System.in);
-        int res1 = sc.nextInt();
-        if (res1 == num1 * num2) {
-            System.out.println("Good Job!!");
-        } else {
-            System.out.println("wrong...try again!!");
+
+        int d = 0;
+        while (d != 1) {
+            int res = sc.nextInt();
+            if (res != num1 * num2) {
+                System.out.println("Please, try again.");
+
+            } else {
+                d = 1;
+                System.out.println("Good Job!!");
+
+
+            }
         }
+
         System.out.println("Let's test your programming knowledge.");
         System.out.println("Why do we use methods?");
         System.out.println("1. To repeat a statement multiple times.");
         System.out.println("2. To decompose a program into several small subroutines.");
         System.out.println("3. To determine the execution time of a program.");
         System.out.println("4. To interrupt the execution of a program.");
+        int c = 0;
+        while (c != 1) {
+            int choice = sc.nextInt();
+            if (choice != 2) {
+                System.out.println("Please, try again.");
 
-
-
+            } else {
+                c = 1;
+            }
+        }
     }
+
 }
