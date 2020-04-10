9ae12c014805159c12e465d71db4eaba1a8ab082
naman <naman.gupta_cs18@gla.ac.in>
2020-04-10 15:02:25 +0530

    calling greet method from Task1 class.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,15 @@ package main;
 
 import task1.Task1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Task1 task1 = new Task1();
         task1.greet("Aid", 2018);
+        //Task2 task2 = new Task2(sc.nextInt);
+
 
 
 
